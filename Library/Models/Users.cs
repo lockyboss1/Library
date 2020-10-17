035e8f308be75a9db6a960c8a00c360e2a2a2712
Ayomide <sonugaengineer@gmail.com>
2020-10-17 16:10:39 +0100

    v2

--- Library/Models/Users.cs
+++ Library/Models/Users.cs
@@ -8,10 +8,20 @@ namespace Library.Models
         [Key]
         public int ID { get; set; }
 
+        [Required]
         public string Name { get; set; }
+
+        [Required]
         public string Author { get; set; }
+
+        [Required]
+        [Range(1, 2020)]
         public int PageCount { get; set; }
+
+        [Required]
+        [Range(1, 2020)]
         public int Year { get; set; }
+
         public bool Read { get; set; }
     }
 }
