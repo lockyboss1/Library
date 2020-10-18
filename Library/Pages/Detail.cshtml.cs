9f94e8353a067f92f45e5854fd725bdd94bb9c7f
Ayomide <sonugaengineer@gmail.com>
2020-10-18 13:11:23 +0100

    v3

---
+++ Library/Pages/Detail.cshtml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Library.Data;
+using Library.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+
+namespace Library.Pages
+{
+    public class DetailModel : PageModel
+    {
+        private readonly UserContext _context;
+
+        public DetailModel(UserContext context)
+        {
+            _context = context;
+        }
+
+        public new Users User { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int id)
+        {
+            User = await _context.User.FindAsync(id);
+
+            if (User == null)
+            {
+                return NotFound();
+            }
+
+            User = await _context.User.FirstOrDefaultAsync(m => m.ID == id);
+
+            return Page();
+        }
+    }
+}
--- Library/Pages/Index.cshtml.cs
+++ Library/Pages/Index.cshtml.cs
@@ -8,12 +8,16 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace Library.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly UserContext _context;
+
+        [BindProperty(SupportsGet = true)]
+        public string SearchString { get; set; }
 
         public IndexModel(UserContext context)
         {
@@ -22,9 +26,19 @@ namespace Library.Pages
 
         public new List<Users> User { get; set; }
 
+        //queries the database to search for user input
+        //searches by name and author
         public async Task OnGetAsync()
         {
-            User = await _context.User.ToListAsync();
+            var books = from b in _context.User
+                        select b;
+
+            if (!string.IsNullOrEmpty(SearchString))
+            {
+                books = books.Where(b => b.Name.ToLower().Contains(SearchString.ToLower()));
+            }
+
+            User = await books.ToListAsync();
         }
 
         public async Task<IActionResult> OnPostAsync(int id)
